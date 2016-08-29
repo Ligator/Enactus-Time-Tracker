27ac4d170ad3a69faf43b69dc639276417e648a9
Your Name <abelpacheco@outlook.com>
2016-08-29 02:39:03 -0500

    Emails setup for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,6 @@ Rails.application.configure do
   #   authentication:       'plain',
   #   enable_starttls_auto: true
   # }
-  config.action_mailer.default_url_options = { :host => 'enactusutm.herokuapp.com' }
 
   config.action_mailer.delivery_method = :sendmail
   # Defaults to:
@@ -85,6 +84,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.default_url_options = { :host => 'enactusutm.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
