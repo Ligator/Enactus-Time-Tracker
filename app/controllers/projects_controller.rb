ef84d7ead07f2207164312a9c41d5007a0e61ba6
Your Name <abelpacheco@outlook.com>
2016-08-29 03:39:11 -0500

    Add :image_url field to Project model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -77,7 +77,7 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :description, :manager_id)
+      params.require(:project).permit(:name, :description, :image_url)
     end
 
     def check_premission
