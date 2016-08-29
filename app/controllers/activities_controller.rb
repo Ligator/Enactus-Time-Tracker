c929bafe976a1767b552693b5d6daa3c417b84b3
Your Name <abelpacheco@outlook.com>
2016-08-29 01:25:49 -0500

    Activities#index and their strong parameters

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,25 @@
 class ActivitiesController < ApplicationController
+  before_action :authenticate_user!
   before_filter :check_premission
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
   # GET /activities
   # GET /activities.json
   def index
-    @activities = Activity.all
+    if current_user.leader?
+      @activities = Activity.for_user(current_user).group_by(&:project_id)
+      @project = current_user.project
+    elsif current_user.admin?
+      @activities = Activity.all.group_by(&:project_id)
+      @project_names = {}
+      Project.select(:id, :name).each{ |project| @project_names[project.id] = project.name }
+    end
+
+    @hours_per_activity = {}
+    HourRecord.where(activity_id: @activities.values.flatten.map(&:id)).each do |hour_record|
+      @hours_per_activity[hour_record.activity_id] ||= 0
+      @hours_per_activity[hour_record.activity_id] += hour_record.worked_hours
+    end
   end
 
   # GET /activities/1
@@ -25,11 +39,12 @@ class ActivitiesController < ApplicationController
   # POST /activities
   # POST /activities.json
   def create
+    ap activity_params
     @activity = Activity.new(activity_params)
 
     respond_to do |format|
       if @activity.save
-        format.html { redirect_to @activity, notice: 'Activity was successfully created.' }
+        format.html { redirect_to activities_url, notice: 'Activity was successfully created.' }
         format.json { render :show, status: :created, location: @activity }
       else
         format.html { render :new }
@@ -43,7 +58,7 @@ class ActivitiesController < ApplicationController
   def update
     respond_to do |format|
       if @activity.update(activity_params)
-        format.html { redirect_to @activity, notice: 'Activity was successfully updated.' }
+        format.html { redirect_to activities_url, notice: 'Activity was successfully updated.' }
         format.json { render :show, status: :ok, location: @activity }
       else
         format.html { render :edit }
@@ -69,12 +84,18 @@ class ActivitiesController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def activity_params
+    def activity_strong_params
       params.require(:activity).permit(:name, :description, :project_id)
     end
 
+    def activity_params
+      params_hash = activity_strong_params.to_hash.symbolize_keys
+      params_hash[:project_id] = current_user.project_id
+      params_hash
+    end
+
     def check_premission
-      unless current_user and current_user.leader?
+      unless current_user.admin? or current_user.leader?
         redirect_to :root
       end
     end
