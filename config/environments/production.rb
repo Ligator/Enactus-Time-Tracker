7c358d08115d787fb0e565a8a1a312d30b25c4bf
Your Name <abelpacheco@outlook.com>
2016-08-29 02:37:03 -0500

    Email setup for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,19 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.default_url_options = { :host => 'enactusutm.heroku.com' }
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'enactusutm.org',
+  #   user_name:            ENV['GMAIL_EMAIL'],
+  #   password:             ENV['GMAIL_PASSWORD'],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
+  config.action_mailer.default_url_options = { :host => 'enactusutm.herokuapp.com' }
+
   config.action_mailer.delivery_method = :sendmail
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
@@ -87,3 +99,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+export GMAIL_EMAIL="pachecos.corp@gmail.com"
+export GMAIL_PASSWORD="ligator123456"
+
