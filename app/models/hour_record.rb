c20ce1367eca8effcbe52b35b1acddcd252bb685
Your Name <abelpacheco@outlook.com>
2016-09-05 01:54:24 -0500

    Add scopes to HourRecord model

--- app/models/hour_record.rb
+++ app/models/hour_record.rb
@@ -1,4 +1,8 @@
 class HourRecord < ActiveRecord::Base
   belongs_to :activity
   belongs_to :user
+
+  scope :for_user,         -> (user) { where(user_id: user.id) }
+  scope :without_activity, -> { where(activity_id: nil) }
+  scope :with_activity,    -> { where("activity_id IS NOT NULL") }
 end
